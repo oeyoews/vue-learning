29541172f8f6586589c98b7db00cb90922945725
oeyoews <jyao4783@gmail.com>
2023-12-13 23:04:15 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -11,56 +11,60 @@ import autoprefixer from 'autoprefixer';
 import VueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  esbuild: {
-    drop: ['console', 'debugger'],
-  },
-  build: {
-    minify: 'terser',
-    ssr: true,
-    terserOptions: {
-      compress: {
-        pure_funcs: ['console.log'],
+export default defineConfig(({ mode }) => {
+  return {
+    esbuild: {
+      // drop: ['console', 'debugger'],
+      pure: mode === 'production' ? ['console.log'] : [],
+      // pure: ['console.log'],
+    },
+    build: {
+      minify: 'terser',
+      ssr: true,
+      terserOptions: {
+        compress: {
+          pure_funcs: ['console.log'],
+        },
+      },
+      sourcemap: false,
+    },
+    plugins: [
+      VueJsx(),
+      VueRouter({
+        dts: 'src/typed-router.d.ts',
+      }), // first
+      Components({
+        extensions: ['vue', 'jsx', 'tsx'],
+        dts: 'src/components.d.ts',
+      }),
+      AutoImport({
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue\??/, // .vue
+        ],
+        imports: ['vue', 'pinia', VueRouterAutoImports],
+        dts: 'src/auto-imports.d.ts',
+        eslintrc: {
+          enabled: false,
+        },
+        dirs: ['src/composables', 'src/stores'],
+        vueTemplate: true,
+      }),
+      vue(),
+    ],
+    server: {
+      port: 3000,
+      open: true,
+    },
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
       },
     },
-    sourcemap: false,
-  },
-  plugins: [
-    VueJsx(),
-    VueRouter({
-      dts: 'src/typed-router.d.ts',
-    }), // first
-    Components({
-      extensions: ['vue', 'jsx', 'tsx'],
-      dts: 'src/components.d.ts',
-    }),
-    AutoImport({
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue\??/, // .vue
-      ],
-      imports: ['vue', 'pinia', VueRouterAutoImports],
-      dts: 'src/auto-imports.d.ts',
-      eslintrc: {
-        enabled: false,
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
       },
-      dirs: ['src/composables', 'src/stores'],
-      vueTemplate: true,
-    }),
-    vue(),
-  ],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
     },
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
+  };
 });
