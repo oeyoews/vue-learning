71e638f038df6b0bd3ff37c3bddc81245566892c
oeyoews <jyao4783@gmail.com>
2023-12-13 21:22:18 +0800

    update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,6 +8,7 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
 	// NOTE: https://stackoverflow.com/questions/73517763/unable-to-use-defineprops-with-typescript
 	// NOTE: https://stackoverflow.com/questions/69951687/vue-3-defineprops-are-referencing-locally-declared-variables
+	/** @see-also: https://cn.vuejs.org/guide/typescript/composition-api.html#props-default-values */
 	links: () => config.links
 })
 
