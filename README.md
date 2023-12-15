2e9c3a6b499e9b61a2366191309c04f1e682d785
oeyoews <jyao4783@gmail.com>
2023-12-15 13:10:38 +0800

    feat: support medium zoom

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has a
 ## TODO
 
 - [ ] 如何共享状态 保持
+- [ ] ssr???(nuxt???)
--- src/pages/learning/conditional.vue
+++ src/pages/learning/conditional.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
 import img from '~/assets/learning/conditional.png';
+import mediumZoom from 'medium-zoom';
+
 const seel = ref(true);
 
 const toggleSeel = () => {
   seel.value = !seel.value;
 };
+
+const getSystemMode = () => {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  }
+};
+
+const systemMode = getSystemMode();
+
+onMounted(() => {
+  mediumZoom('.zoomable-image', {
+    background:
+      systemMode === 'dark' ? 'rgba(0, 0, 0, 0.6)' : 'rgba(255, 255, 255, 0.6)',
+    scrollOffset: 0,
+  });
+});
 </script>
 
 <template>
@@ -12,7 +30,19 @@ const toggleSeel = () => {
   <p v-if="seel" class="font-bold">可见</p>
   <del v-else>可见</del>
 
-  <img :src="img" class="rounded w-96" />
+  <img :src="img" class="rounded w-96 zoomable-image" />
+
+  <h3>v-show/v-if</h3>
+  <p>
+    v-if
+    是“真实的”按条件渲染，因为它确保了在切换时，条件区块内的事件监听器和子组件都会被销毁与重建。
+    v-if 也是惰性的：如果在初次渲染时条件值为
+    false，则不会做任何事。条件区块只有当条件首次变为 true 时才被渲染。
+    相比之下，v-show 简单许多，元素无论初始条件如何，始终会被渲染，只有 CSS
+    display 属性会被切换。 总的来说，v-if 有更高的切换开销，而 v-show
+    有更高的初始渲染开销。因此，如果需要频繁切换，则使用 v-show
+    较好；如果在运行时绑定条件很少改变，则 v-if 会更合适。
+  </p>
   <p>v-if, v-else, v-if-else</p>
 
   <button @click="toggleSeel">toggle</button>
