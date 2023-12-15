973c294b4e0d914290a4db1ffc05b8ae594e0e24
oeyoews <jyao4783@gmail.com>
2023-12-15 21:12:43 +0800

    feat: support nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,30 @@
-import { createRouter, createWebHistory } from "vue-router/auto";
-import { setupLayouts } from "virtual:generated-layouts";
+import { createRouter, createWebHistory } from 'vue-router/auto';
+import { setupLayouts } from 'virtual:generated-layouts';
+import NProgress from 'nprogress';
+import '../../node_modules/nprogress/nprogress.css';
 
-import { routes } from "vue-router/auto/routes";
+import { routes } from 'vue-router/auto/routes';
 
+NProgress.configure({
+  showSpinner: false,
+});
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: () => setupLayouts(routes),
+});
+
+router.beforeResolve((to, _, next) => {
+  // If this isn't an initial page load.
+  if (to.name) {
+    // Start the route progress bar.
+    NProgress.start();
+  }
+  next();
+});
+
+router.afterEach(() => {
+  // Complete the animation of the route progress bar.
+  NProgress.done();
 });
 
 export default router;
